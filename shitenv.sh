f882a4ca3730a41f6338c59a7d5a4ab8509c1551
oott123 <git@public.oott123.com>
2019-08-23 17:21:58 +0800

    feat: auto completion

--- shitenv.sh
+++ shitenv.sh
@@ -6,6 +6,9 @@ if [ "$S__TENV_ROOT" != "" ] ; then
         local cur=${COMP_WORDS[COMP_CWORD]}
         COMPREPLY=( $(compgen -W "`ls $S__TENV_ROOT/env`" -- $cur) )
     }
+    if [[ -n ${ZSH_VERSION-} ]]; then
+        autoload -U +X bashcompinit && bashcompinit
+    fi
     complete -F _shitenv_comp shitenv
 else
     echo "missing \$S__TENV_ROOT"
