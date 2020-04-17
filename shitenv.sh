ae036c1ff590aebcbfc5eca6ee1fe79419a3f2e1
oott123 <git@public.oott123.com>
2020-04-18 00:53:13 +0800

    feat: add env for env dir

--- shitenv.sh
+++ shitenv.sh
@@ -1,15 +1,17 @@
 if [ "$S__TENV_ROOT" != "" ] ; then
+    export S__TENV_ENV="${S__TENV_ENV:-$S__TENV_ROOT/env}"
     function shitenv () {
-        source $S__TENV_ROOT/env/$1
+        source $S__TENV_ENV/$1
     }
     function _shitenv_comp () {
         local cur=${COMP_WORDS[COMP_CWORD]}
-        COMPREPLY=( $(compgen -W "`ls $S__TENV_ROOT/env`" -- $cur) )
+        COMPREPLY=( $(compgen -W "`ls $S__TENV_ENV/`" -- $cur) )
     }
     if [[ -n ${ZSH_VERSION-} ]]; then
         autoload -U +X bashcompinit && bashcompinit
     fi
     complete -F _shitenv_comp shitenv
 else
-    echo "missing \$S__TENV_ROOT"
+    echo "[shitenv] missing \$S__TENV_ROOT"
 fi
+
